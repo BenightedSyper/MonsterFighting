fc5c0dfd2f238fb9018a72623f9f96ddb330f88a
CaptonCrysco <73364967+CaptonCrysco@users.noreply.github.com>
2020-10-29 06:06:38 -0600

    ruines

---
+++ Assets/Gear.cs
@@ -0,0 +1,26 @@
+/*Just thinking in code form
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public Double subSlotone;
+public Double subSlottwo;
+public Double subSlotthree;
+public Double subSlotfour;
+
+public int[2] gearLevelone;         //no subs capable
+public int[3] gearLeveltwo;         //one sub possable
+public int[4] gearLevelthree;
+public int[5] gearLevelfour;
+public int[6] gearLevelfive;
+
+
+
+public struct Helmet{                           //replace slot 5
+
+}
+
+
+*/
