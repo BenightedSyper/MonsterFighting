2040cab746a99f1db30d13df6745d10e935501b1
BenightedSyper <devek.paid@gmail.com>
2020-11-08 16:14:31 -0700

    attack and defense break
    added some visuals to attack and defense break

--- Assets/MonsterStatusManager.cs
+++ Assets/MonsterStatusManager.cs
@@ -8,6 +8,9 @@ public class MonsterStatusManager : MonoBehaviour
     public Monster myMonster;
     public Slider HealthBar;
     public Slider AttackBar;
+    public Texture defenseBreak;
+    public Texture attackBreak;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +24,19 @@ public class MonsterStatusManager : MonoBehaviour
         }
         HealthBar.value = myMonster.GetPercentHealth();
         AttackBar.value = myMonster.GetPercentAttackBar();
+    }
+    void OnGUI(){
+        foreach (StatusEffect se in myMonster.statusEffects){
+            switch (se.statusType){
+                case STATUSEFFECTTYPE.DEFENSEBREAK:
+                    Graphics.DrawTexture(new Rect(10, 10, 100, 100), defenseBreak);
+                    break;
+                case STATUSEFFECTTYPE.ATTACKBREAK:
+                    Graphics.DrawTexture(new Rect(110, 10, 100, 100), attackBreak);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
