8b8119ea8a7a3bba8ab918ccd645cd03d0570cf6
CaptonCrysco <73364967+CaptonCrysco@users.noreply.github.com>
2020-12-01 06:30:47 -0700

    started to try and build menus

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    //eventually if there was a server this would take you to a login remember a loading screen
+    public void LoginButton(){
+        SceneManager.LoadScene(1);
+    }
+    //eventually if there was a server this would take you to a login remember a loading screen
+    public void NewAccountButton(){ 
+        SceneManager.LoadScene(1);
+    }
+}
